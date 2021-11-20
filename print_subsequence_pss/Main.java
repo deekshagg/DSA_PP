215409165ed2f5f6d06943a5717dfcd26ebeb5cb
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-11-20 23:26:29 +0530

    print subsequence

---
+++ print_subsequence_pss/Main.java
@@ -0,0 +1,23 @@
+package print_subsequence_pss;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        print_Subsequence(string, "");
+    }
+    public static void print_Subsequence(String queString, String answer_so_far){
+        if(queString.length() == 0){
+            System.out.println(answer_so_far);
+            return;
+        }
+
+        char ch = queString.charAt(0); //x
+        String restOfQues = queString.substring(1); //yz
+        print_Subsequence(restOfQues, answer_so_far); // "-" -> chahiye nhi hota
+        print_Subsequence(restOfQues, answer_so_far + ch);
+    }
+}
