aaba92200c271ab2cee4851919a232609ce8af5e
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-11-22 20:57:35 +0530

    print stair paths

---
+++ recursion/on_wayup/print_Stair_paths/Main.java
@@ -0,0 +1,25 @@
+package recursion.on_wayup.print_Stair_paths;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        printStairPaths(n,"");
+    }
+    public static void printStairPaths(int n, String path){
+
+        if(n<0){
+            return;
+        }
+        else if(n==0){
+            System.out.println(path);
+            return;
+        }
+        printStairPaths(n-1, path + "1");
+        printStairPaths(n-2, path + "2");
+        printStairPaths(n-3, path + "3");
+        
+    }
+}
