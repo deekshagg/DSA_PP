8ae746a3ad745846ae9ca8c5aad7767c4b1023bf
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-11-25 12:34:14 +0530

    power log

---
+++ power_logarithmic/Main.java
@@ -0,0 +1,29 @@
+package power_logarithmic;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+        int powerLog = power(x , n);
+        System.out.println(powerLog);
+    }
+
+    public static int power(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        
+        int answer = power(x , n/2) * power(x,n/2); 
+
+        if(n%2==1){
+            answer = answer * x;
+        }
+        return answer;
+    }
+
+}
