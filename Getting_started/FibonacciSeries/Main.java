76a46c491a3e712236324125fb7a72f020b3f1ab
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-11-28 21:09:19 +0530

    fibonacci check

--- Getting_started/FibonacciSeries/Main.java
+++ Getting_started/FibonacciSeries/Main.java
@@ -36,3 +36,25 @@ public class Main {
         return fn;
     }
 }
+
+
+// done by me
+// public static void main(String[] args) {
+//     Scanner scn = new Scanner(System.in);
+//     int n = scn.nextInt();
+//     int a = 0;
+//     int b = 1;
+//     int count =  1;
+//     while(count < n){
+//       if(a==0 || b==0){
+//           System.out.println(a);
+//           System.out.println(b);
+//           count=2;
+//       }
+//       int c = a+b;
+//       System.out.println(c);
+//       a= b;
+//       b= c;
+//       count++;
+//     }
+//  }
