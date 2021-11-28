390cf0da334d2b224037c536c460dd0010148c7f
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-11-29 00:35:12 +0530

    reverse digits

---
+++ reverseDigits/Main.java
@@ -0,0 +1,22 @@
+package reverseDigits;
+
+import java.util.*;
+   
+   public class Main{
+   
+   public static void main(String[] args) {
+     Scanner scn = new Scanner(System.in);
+     int n = scn.nextInt();
+     int count = (int) Math.log10(n)+1;
+     reverseDigits(n,count);
+    }
+    public static void reverseDigits(int n , int count){
+      int ans = 0;
+      while(count>0){
+        ans = n % 10;
+        n = n / 10;
+        System.out.println(ans);
+        count--;
+      }
+    }
+   }
