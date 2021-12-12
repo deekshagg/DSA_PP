bf181dec87293a3a17d6d9449795bcb6f53f3626
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-12-12 19:39:17 +0530

    string compression

---
+++ stringCompression/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    
+}
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static String compression1(String str){
+		String newstr = str.charAt(0) + "";
+        for(int i = 1 ; i < str.length() ; i++){
+            char curr = str.charAt(i);
+            char prev = str.charAt(i-1);
+            if(curr!=prev){
+                newstr+=curr;
+            }
+
+        }
+
+		return newstr;
+	}
+
+	public static String compression2(String str){
+		String newstr = str.charAt(0) + "";
+        int count = 1;
+        for(int i = 1 ; i < str.length() ; i++){
+            char curr = str.charAt(i);
+            char prev = str.charAt(i-1);
+            if(curr==prev){
+                count++;
+            }
+            else{
+                if(count>1){
+                    newstr+=count;
+                    count=1;
+                }
+                newstr +=  curr;
+            }
+            
+
+        }
+        if(count>1){
+            newstr+=count;
+            count=1;
+        }
+
+		return newstr;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(compression1(str));
+		System.out.println(compression2(str));
+	}
+
+}
