fb264d9ab2e31162b576b1fecea2bed9906c8b7e
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-12-23 01:45:59 +0530

    coin change combination

--- DynamicProgramming/DynamicAndGreedy/CoinChangeCombination/Main.java
+++ DynamicProgramming/DynamicAndGreedy/CoinChangeCombination/Main.java
@@ -1,3 +1,28 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = scn.nextInt();
+        }
+        int amount = scn.nextInt();
+        coinchangecombinations(n,arr,amount);
+
+    }
+    public static void coinchangecombinations(int n , int[] arr, int amount){
+        int[] dp = new int[amount+1];
+
+        dp[0] = 1;
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = arr[i] ; j < dp.length ; j++){
+                dp[j] += dp[j-arr[i]];
+            }
+        }
+        System.out.println(dp[amount]);
+    }
 }
