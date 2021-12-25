18ae9156fa9cd42b943b54248265dd650af8b9e3
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-12-25 22:36:51 +0530

    rotate array

---
+++ 1_BasicsOfProgramming/FunctionAndArray/Arrays/Rotate/Main.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+
+  public static void rotate(int[] a, int k){
+    // write your code here
+    int n = a.length;
+    k = k % a.length;
+    if(k < 0){
+      k = a.length + k;
+    }
+    reverse(a,0 , n-1-k);
+    reverse(a, n - k , n -1);
+    reverse(a, 0 , n-1);
+
+  }
+
+  public static void reverse(int a[], int left , int right){
+    while(left < right){
+      int temp = a[left];
+      a[left] = a[right];
+      a[right] = temp;
+      left++;
+      right--;
+    }
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+    int k = Integer.parseInt(br.readLine());
+
+    rotate(a, k);
+    display(a);
+ }
+
+}
