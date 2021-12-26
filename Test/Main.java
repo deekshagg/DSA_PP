365ad5f9340ea97b336246b6ec71bd85668a83c2
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-12-26 16:26:25 +0530

    test

---
+++ Test/Main.java
@@ -0,0 +1,12 @@
+// Given a number n, find the highest power of 2 that is smaller than or equal to n.
+
+// Examples : 
+
+// Input : n = 10
+// Output : 8
+
+// Input : n = 19
+// Output : 16
+
+// Input : n = 32
+// Output : 32
---
+++ Test/Main3.java
@@ -0,0 +1,15 @@
+package Test;
+
+// Duplicated and missing number in an array
+// The input nums is supposed to be an array of unique integers
+//  ranging from 1 to nums.length (inclusive). However, there is a mistake: 
+// one of the numbers in the array
+// duplicated, which means another number is missing.
+// Find and return the sum of the duplicated number and the missing number.
+// Example in the array [4, 3, 3, 1], 3is present twice and 2 is missing, 
+// so 3 +2= 5 should be returned.
+ 
+
+public class Main3 {
+    
+}
