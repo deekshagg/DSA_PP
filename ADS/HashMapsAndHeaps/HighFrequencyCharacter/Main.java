81b0cdee0e9b87c29a9ae8f2bf44591ecf0b0ae8
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-12-26 23:37:29 +0530

    high frequency character

--- ADS/HashMapsAndHeaps/HighFrequencyCharacter/Main.java
+++ ADS/HashMapsAndHeaps/HighFrequencyCharacter/Main.java
@@ -1,5 +1,41 @@
 package ADS.HashMapsAndHeaps.HighFrequencyCharacter;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine(); // abbcd
+        printHighFreqChar(str);
+    }
+    public static void printHighFreqChar(String str){
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+        // a    1
+        // b    2
+        // c    1
+        // d    1
+        for(int i = 0 ; i < str.length() ; i++){//0 1 2 3 4
+            char ch = str.charAt(i); //a    b   b    c   d
+            if(hashmap.containsKey(ch)){   // ->no  yes
+                int oldF = hashmap.get(ch); //1
+                int newF = oldF + 1;//2
+                hashmap.put(ch, newF);//->b[2]
+            }
+            else{
+                hashmap.put(ch, 1);   // -> a[1]    ->b[2]  ->c[1]  ->d[1]
+            }
+        }
+        char maxfchar = str.charAt(0);//a   
+        for(Character key : hashmap.keySet()){//a   b   c   d
+            if(hashmap.get(key)>hashmap.get(maxfchar)){// 1>1   2>1     1>2
+                maxfchar = key; // a-> b 
+            }
+        }
+        System.out.println(maxfchar); //b
+
+    }
+
 }
