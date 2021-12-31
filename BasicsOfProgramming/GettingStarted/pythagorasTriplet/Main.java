db900b3fc2b279893fa36af5d2a22ed106c5bc58
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2021-12-31 15:48:08 +0530

    pythagoras triplet

---
+++ BasicsOfProgramming/GettingStarted/pythagorasTriplet/Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+    // write your code here  
+    Scanner scn = new Scanner(System.in);
+    int a = scn.nextInt();
+    int b = scn.nextInt();
+    int c = scn.nextInt();
+    int max = Math.max(a , Math.max(b,c));
+    if(max == a){
+      boolean flag = (b*b + c*c == a*a);
+      System.out.println(flag);
+    }
+    else if(max == b){
+      boolean flag = (a*a + c*c == b*b);
+      System.out.println(flag);
+    }
+    else{
+      boolean flag = (b*b + a*a == c*c);
+      System.out.println(flag);
+    }
+   }
+  }
