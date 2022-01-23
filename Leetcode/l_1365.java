d153f3341c88a05819861507d4bd3ff207e3b3e7
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-01-23 19:27:09 +0530

    1365 How Many Numbers Are Smaller Than the Current Number leetcode

---
+++ Leetcode/l_1365.java
@@ -0,0 +1,16 @@
+package Leetcode;
+class l_1365 {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        
+        for(int i = 0 ; i < nums.length ; i++){
+            int count = 0;
+            for(int j = 0 ; j < nums.length; j++){
+                if(nums[j]<nums[i])
+                    count++;
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
--- Leetcode/l_2114.java
+++ Leetcode/l_2114.java
@@ -4,8 +4,8 @@ class l_2114 {
     public int mostWordsFound(String[] sentences) {
         int count = 0;
         for(int i = 0 ; i < sentences.length ; i++){
-            String string = sentences[i];
-            int c = string.split(" ").length;
+            String string = sentences[i]; // "cake is tasty"
+            int c = string.split(" ").length; // ["cake", "is", "tasty"]
             if(c>count)
                 count = c;
         }
