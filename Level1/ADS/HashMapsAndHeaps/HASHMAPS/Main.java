a45a0457b99dbe515a258173315e69255dbfa3a0
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-01-28 00:53:01 +0530

    kth largest elements heap and priority queue

---
+++ Level1/ADS/HashMapsAndHeaps/HASHMAPS/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // HashMap<Integer, Character>
+    }
+    // HashMap
+    // put() == insert, update (left, right)
+    // get(key) == value(right side)
+    // containsKey(key) ==  boolean
+    // keySet() == set 
+    // remove(key) == deletes the key
+}
