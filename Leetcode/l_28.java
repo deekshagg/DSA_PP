fb8aa28db2f158aa16b51f3a7b76d42347547bf9
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-01-30 00:07:53 +0530

    28 implement strStr

---
+++ Leetcode/l_28.java
@@ -0,0 +1,8 @@
+package Leetcode;
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle == null)  return 0;
+        return haystack.indexOf(needle,0); 
+    }
+}
