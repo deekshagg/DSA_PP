c877ac7f3ce9b738131c643366ca7eb291404b6d
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-01-30 01:01:33 +0530

    search insert position leetcode 35

---
+++ Leetcode/l_35.java
@@ -0,0 +1,20 @@
+package Leetcode;
+
+class l_35 {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        int mid;
+        while(l<=r){
+            mid = (l + r)/2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] < target){
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+        return l;
+    }
+}
