933c391a32446e529e3a2007218681fd7e0c6b36
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-01 14:00:36 +0530

    buy and stockgit add .

--- Leetcode/l_121.java
+++ Leetcode/l_121.java
@@ -1,3 +1,15 @@
-public class l_121 {
-    
+package Leetcode;
+class l_121 {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int bp = prices[0];
+        for(int i = 1; i < prices.length ; i++){
+            int sp = prices[i];
+            if(sp>bp){
+                profit = Math.max(profit , sp-bp);
+            }
+            bp = Math.min(sp, bp);
+        }
+        return profit;
+    }
 }
