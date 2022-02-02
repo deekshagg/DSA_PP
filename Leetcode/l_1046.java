a5db2833f463edc9555a44fd9582736214ee10b6
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-03 01:47:28 +0530

    1046. Last Stone Weight leetcode

---
+++ Leetcode/l_1046.java
@@ -0,0 +1,16 @@
+package Leetcode;
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq  = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0 ; i < stones.length ; i++){
+            pq.add(stones[i]);
+        }
+        while(pq.size()>1){
+            int temp = pq.remove();
+            int temp2 = pq.remove();
+            int ans = temp - temp2;
+            pq.add(ans);
+        }
+        return pq.peek();
+    }
+}
