96898792e505b0e82f8fe3c16d3088e6b7949cf5
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-08 02:53:43 +0530

    110 balanced binary tree method 2 bottom up aproach

--- Leetcode/l_110.java
+++ Leetcode/l_110.java
@@ -1,18 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+//  Definition for a binary tree node.
+  class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
 class l_110 {
     public boolean isBalanced(TreeNode root) {
         // top - down approach 
@@ -25,5 +25,22 @@ class l_110 {
     public int height(TreeNode root){
         if(root == null) return 0;
         return Math.max(height(root.left),height(root.right))+1;
+    }
+}
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        // bottom - up approach 
+        // tc = O(n)
+        if(root == null) return true;
+        return height(root) != -1;
+    }
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        int balanced_factor = Math.abs(left-right);
+        if(balanced_factor > 1 || left == -1 || right == -1) return -1;
+        return Math.max(left, right)+1;
     }
 }
