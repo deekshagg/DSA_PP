425cf77e3f692f8fa48e43ab1eb599248f514418
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-08 06:31:53 +0530

    2nd approach to find digital root

--- Leetcode/l_258.java
+++ Leetcode/l_258.java
@@ -8,3 +8,20 @@ class l_258 {
         return num%9==0?9:num%9;
     }
 }
+
+class Solution {
+    public int addDigits(int num) {
+        if(num==0) return num;
+        // naive solution
+        int digital_root = 0;
+        while(num>0){
+            digital_root += num%10;
+            num/=10;
+            if(num==0&&digital_root>9){
+                num = digital_root;
+                digital_root = 0;
+            }
+        }
+        return digital_root;
+    }
+}
