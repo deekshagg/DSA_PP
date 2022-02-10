74d3b634e8b29ee23fc04663a771d82e4610d67c
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-11 01:23:16 +0530

    560. Subarray Sum Equals K leetcode

---
+++ Leetcode/l_560.java
@@ -0,0 +1,35 @@
+package Leetcode;
+
+class l_560 {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0 ; 
+        for(int i = 0 ; i < nums.length ; i++){
+            int sum = 0; 
+            for(int j = i ; j < nums.length ; j++){
+                sum += nums[j];
+                if(sum==k) count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0; 
+        HashMap<Integer , Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            sum+=nums[i];
+            if(map.containsKey(sum -k))
+                count+= map.get(sum-k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+                
+        }
+        return count;
+        // time complexity O(n)
+        //space complexity  O(n)
+    }
+}
