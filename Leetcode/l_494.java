7999249cf0959455500640411962b349445a3e65
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-11 08:29:06 +0530

    494. Target Sum leetcode

---
+++ Leetcode/l_494.java
@@ -0,0 +1,13 @@
+class l_494 {
+    public int findTargetSumWays(int[] nums, int target) {
+        return targetSum(nums, 0, 0, target);
+    }
+    public int targetSum(int[] arr, int idx, int sos, int tar){
+        if(arr.length == idx){
+            return sos == tar?1:0;
+        }
+        
+        return targetSum(arr, idx+1, sos-arr[idx], tar)+
+        targetSum(arr, idx+1, sos+arr[idx], tar);
+    }
+}
