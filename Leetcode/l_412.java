eccab4908f92ae4607ca8e0727891add6498db4e
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-15 00:29:21 +0530

    412. Fizz Buzz leetcode

---
+++ Leetcode/l_412.java
@@ -0,0 +1,17 @@
+class l_412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for(int i = 1 ; i <= n ; i++){
+            String s = "";
+            if(i%3 == 0 && i%5 ==0)
+                s = "FizzBuzz";
+            else if(i%3 == 0)
+                s = "Fizz";
+            else if(i%5 == 0)
+                s = "Buzz";
+            else s = Integer.toString(i);
+            list.add(s);
+        }
+        return list;
+    }
+}
