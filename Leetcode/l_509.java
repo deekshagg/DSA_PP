81d4fa3a89adc034c2d19511c5ae895ecc846cd9
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-02-19 01:27:59 +0530

    dp fibonacci

--- Leetcode/l_509.java
+++ Leetcode/l_509.java
@@ -5,3 +5,15 @@ class l_509 {
         return fib(n-1)+fib(n-2);
     }
 }
+
+class Solution {
+    public int fib(int n) {
+        int dp[] = new int[31];
+        
+        if(n<=1) return n;
+        if(dp[n] > 0) return dp[n];
+        
+        dp[n] = fib(n-1) + fib(n-2);
+        return dp[n];
+    }
+}
