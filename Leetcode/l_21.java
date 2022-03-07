8c577c1a3131cfb61857ae76b061698594e063d4
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-03-07 17:39:20 +0530

    Subarray of array

--- Leetcode/l_21.java
+++ Leetcode/l_21.java
@@ -1,4 +1,4 @@
-package Leetcode;
+package LeetCode;
 
 /**
  * Definition for singly-linked list.
--- Test/Main.java
+++ Test/Main.java
@@ -1,10 +1,11 @@
-mport java.io.*;
+
+import java.io.*;
 import java.util.*;
 
 public class Main{
   
 
-    public static void main(String[] args) throws Exception {
+    static public void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String exp = br.readLine();
 
