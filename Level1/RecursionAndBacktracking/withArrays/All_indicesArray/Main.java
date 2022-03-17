5dd81c306a9c1eb74efcb5369021271fc3fc554d
Diksha Aggarwal <agarwal.deeksha498@gmail.com>
2022-03-17 10:04:01 +0530

    all indices of array

--- Level1/RecursionAndBacktracking/withArrays/All_indicesArray/Main.java
+++ Level1/RecursionAndBacktracking/withArrays/All_indicesArray/Main.java
@@ -1,5 +1,3 @@
-package recursion.withArrays.All_indicesArray;
-
 import java.io.*;
 import java.util.*;
 
@@ -27,17 +25,18 @@ public class Main {
     }
 
     public static int[] allIndices(int[] arr, int x, int idx, int fsf) {
-        if(idx==arr.length){
-           return new int [fsf];
+        if(idx == arr.length) {
+            if(fsf == 0) System.out.println("NO OUTPUT");
+            return new int[fsf];
         }
-        if(x == arr[idx]){
-           int[] iarr = allIndices(arr , x, idx + 1 , fsf+1);
-           iarr[fsf] = idx;
-           return iarr;
+        if(arr[idx] == x){
+            int[] iarr = allIndices(arr, x, idx+1, fsf+1);
+            iarr[fsf] = idx;
+            return iarr;
         }
         else{
-            int[] iarr = allIndices(arr , x, idx + 1 , fsf);
-           return iarr;
+            int[] iarr = allIndices(arr, x, idx+1, fsf);
+            return iarr;
         }
     }
 
